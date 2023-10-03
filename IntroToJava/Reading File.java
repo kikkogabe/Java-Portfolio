72ee69823bbd7f995ddad9984848b978a950becd
kikkogabe <146867084+kikkogabe@users.noreply.github.com>
2023-10-03 16:41:10 -0400

    Introduction to Reading Files

---
+++ IntroToJava/Reading File.java
@@ -0,0 +1,44 @@
+/**
+ * Program to copy one file into another, using command line arguments
+ *
+ * @author YOUR NAME HERE
+ * @author YOUR PARTNER NAME HERE
+ * @version DATE HERE
+ *
+ */
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class FunWithFiles {
+   
+   /**
+    * Displays the file given by fname to the screen
+    * 
+    * @param fname file to be displayed
+    */
+   public static void displayFile(String fname) {
+        FileInputStream fileByteStream = null; // File input stream
+        Scanner inFS = null; 
+        try {
+            fileByteStream = new FileInputStream(fname);
+            inFS = new Scanner (fileByteStream);
+            while (inFS.hasNext()) {
+                System.out.println (inFS.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println ("ERROR - File " + fname + " not found!");
+        }
+    }
+   
+    public static void main(String[] args) {
+        String fname;
+        Scanner scnr = new Scanner(System.in);
+        System.out.print ("Enter a filename: ");
+        fname = scnr.next();
+        displayFile(fname);  
+        scnr.close();
+    }
+}
